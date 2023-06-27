762d440943f82128ea82c99c2d873898cc50c4d1
Anmolqq <basnetanmol2020@gmail.com>
2023-06-27 18:20:25 +0545

    addes deploy logic

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/React_tic/'
+  }
+
+  return config
 })
